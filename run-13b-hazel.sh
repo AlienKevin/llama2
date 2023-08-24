407750b01f400a009d781d37185f0bdb7076ae13
Kevin Li <kevinli020508@gmail.com>
2023-08-24 14:54:55 -0400

    Add shell script to run with hazel grammar

---
+++ run-13b-hazel.sh
@@ -0,0 +1,14 @@
+cd llama.cpp
+
+# Run in interactive mode
+./main -m ./models/llama-2-13b-chat.ggmlv3.q4_0.bin \
+  --grammar-file grammars/hazel.gbnf \
+  --color \
+  --ctx_size 2048 \
+  -n -1 \
+  -ins -b 256 \
+  --top_k 10000 \
+  --temp 0.2 \
+  --repeat_penalty 1.1 \
+  -t 8 \
+  -ngl 1
