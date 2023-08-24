5ccd9141beacb308f14bcb5135bf2dd0191ddd8c
Kevin Li <kevinli020508@gmail.com>
2023-08-24 12:53:22 -0400

    Expand option names in serve-13b.sh

--- serve-13b.sh
+++ serve-13b.sh
@@ -3,8 +3,8 @@ cd llama.cpp
 # Run in interactive mode
 ./server -m ./models/llama-2-13b-chat.ggmlv3.q4_0.bin \
   --ctx_size 2048 \
-  -t 8 \
-  -ngl 1 \
-  -eps 1e-5 \
-  -b 256 \
+  --threads 8 \
+  --n-gpu-layers 1 \
+  --rms-norm-eps 1e-5 \
+  --batch-size 256 \
   -gqa 1
