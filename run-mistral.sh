c334d1212cc5ac077c8c98324d313f70ff40396f
Kevin Li <kevinli020508@gmail.com>
2023-10-25 16:31:01 -0400

    Add mistral command

--- run-mistral.sh
+++ run-mistral.sh
@@ -1,15 +1,14 @@
 cd llama.cpp
 
-# --grammar-file ../hazel.gbnf \
-
 ./main \
+    --grammar-file ../hazel.gbnf \
     -t 10 \
     -ngl 64 \
     -b 512 \
-    -m ../models/codellama-34b-instruct.Q5_K_M.gguf \
+    -m ../models/mistral-7b-v0.1.Q5_K_S.gguf \
     --color -c 3100 \
     --temp 0.7 \
     --repeat_penalty 1.1 \
     -n -1 \
-    -i -ins \
-    -f ../prompt_adt-instruct.txt
+    -i -ins\
+    -f ../prompt_adt.txt
