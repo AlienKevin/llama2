110ee75db955bcb7f7c5981e55ab47aae4a5e3c8
Kevin Li <kevinli020508@gmail.com>
2023-10-30 10:15:54 -0400

    Fuse query into prompt

--- run-code-34b.sh
+++ run-code-34b.sh
@@ -6,9 +6,8 @@ cd llama.cpp
     -ngl 64 \
     -b 512 \
     -m ../models/codellama-34b.Q5_K_M.gguf \
-    --color -c 3100 \
+    --color -c 2000 \
     --temp 0.7 \
     --repeat_penalty 1.1 \
     -n -1 \
-    -i -ins\
-    -f ../prompt_adt.txt
+    -f ../prompt_adt-autoregressive.txt
