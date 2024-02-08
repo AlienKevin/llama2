aee5f82c655a39f56a1cdb9288831f6bf3273064
Kevin Li <kevinli020508@gmail.com>
2024-02-08 00:44:38 -0500

    Add run-all script

---
+++ run-all.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Navigate to the directory containing the main executable and queries
+cd llama.cpp
+
+# Ensure outputs directory exists
+mkdir -p ../outputs
+
+# Define an array of file paths
+# query_files=(../queries/todo.toggle.txt ../queries/todo.update.txt)
+
+# Loop through all .txt files in the queries directory
+# for query_file in "${query_files[@]}"; do
+for query_file in ../queries/*.txt; do
+    # Extract the name of the query without the path and extension
+    query_name=$(basename "$query_file" .txt)
+    echo "Testing ${query_name}"
+
+    cd ../
+    python expand_prompt.py autoregressive "${query_name}"
+    cd llama.cpp
+
+
+    # Clears log
+    touch log.txt && echo "" truncate -s 0 log.txt
+
+    # Run the main command with modifications for each query
+    ./main \
+        --dynamic-grammar context \
+        -t 10 \
+        -ngl 64 \
+        -b 512 \
+        -m ../models/codellama-34b.Q5_K_M.v3.gguf \
+        --color -c 3400 \
+        --seed 1 \
+        --temp 0 \
+        --repeat_penalty 1.1 \
+        -n -1 \
+        -f "../autoregressive.prompt" \
+        --prelude "../autoregressive.prelude"
+
+    # After running the command, move and rename log.txt to the outputs folder with a related name
+    mv log.txt "../outputs/${query_name}_log.txt"
+done
