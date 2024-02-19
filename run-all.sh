33bc3ee1582b572e93bcf05ed9797a9fec623653
Kevin Li <kevinli020508@gmail.com>
2024-02-19 09:51:27 -0500

    Update outputs

--- run-all.sh
+++ run-all.sh
@@ -7,13 +7,23 @@ cd llama.cpp
 mkdir -p ../outputs
 
 # Define an array of file paths
-# query_files=(../queries/todo.toggle.txt ../queries/todo.update.txt)
+query_files=(../queries/playlist.txt ../queries/todo.add.txt ../queries/todo.remove.txt ../queries/todo.toggle.txt ../queries/todo.update.txt)
+
+skip_files=("list.filter_map_int", "list.find_int")
 
 # Loop through all .txt files in the queries directory
-# for query_file in "${query_files[@]}"; do
-for query_file in ../queries/*.txt; do
+for query_file in "${query_files[@]}"; do
+# for query_file in ../queries/*.txt; do
     # Extract the name of the query without the path and extension
     query_name=$(basename "$query_file" .txt)
+
+    for i in "${skip_files[@]}"; do
+        if [[ "$i" == "$query_name" ]]; then
+            echo "Skipping ${query_name}"
+            continue 2
+        fi
+    done
+
     echo "Testing ${query_name}"
 
     cd ../
@@ -26,7 +36,7 @@ for query_file in ../queries/*.txt; do
 
     # Run the main command with modifications for each query
     ./main \
-        --dynamic-grammar context \
+        --dynamic-grammar types \
         -t 10 \
         -ngl 64 \
         -b 512 \
